63f3b6ce83fdc8e5f4f7a8465935757fd9b7c145
Emir Koroglu <69024539+emirsway@users.noreply.github.com>
2021-12-09 16:29:37 -0600

    Create docker-image-script.sh

---
+++ docker-image-script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo Please tell me the image.
+
+read IMAGE
+
+docker run -d --name task -P  $IMAGE  2> /dev/null
+
+docker ps 
+
+echo Please tell me the running container id.
+
+read CONTAINERID
+
+docker exec -it "$CONTAINERID"  bash   2> /dev/null
