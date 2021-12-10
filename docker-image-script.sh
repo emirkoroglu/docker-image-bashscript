1975ee8aa6ee6c289a93002374c01fb995e1c4a9
Emir Koroglu <69024539+emirsway@users.noreply.github.com>
2021-12-10 02:08:47 -0600

    Update docker-image-script.sh

--- docker-image-script.sh
+++ docker-image-script.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-echo Please tell me the image.
+echo Please Enter the Docker Image
 
 read IMAGE
-echo Please wait
+echo Please Wait!
 docker run -d --name task -P  $IMAGE  2> /dev/null
 
 docker ps 
 
-echo Please tell me the running container id.
+echo Please enter the running Container ID
 
 read CONTAINERID
 
 docker exec -it "$CONTAINERID"  bash   2> /dev/null
 
-sleep 10
+sleep 1
 
-echo Would you like to delete all images and containers? only yes will be accepted
-read yes
-if  [ $yes = "yes" ]
+echo Warning! Would you like to delete all images and containers? only y will be accepted
+read y
+if  [ $y = "y" ]
 then
   docker rm -f `docker ps -qa` 
   docker rmi -f $(docker images -aq)
 else
-  echo "Destroy cancelled"
+  echo "Destroy Cancelled"
 fi
 
 #emirsway copyright 2021
